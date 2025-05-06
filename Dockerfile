d1a69fa6b74248084308c798bf8acb97fa9270de
Jmserge29 <Jmserge29@gmail.com>
2025-05-05 19:19:47 -0500

    Fixed: limpiando el espacio de trabajo

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.10
-WORKDIR /app
-COPY . .
-RUN pip install --no-cache.dir -r requiriments.txt 
-
-EXPOSE 5000
-
-CMD [ "python" , "app/app.py"]
--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, redirect, render_template
-from config.db import app
-
-@app.route("/")
-def index():
-    return render_template("index.htl")
-
-if __name__ == "__main__":
-    app-run(debud=True, port=5000, host="0.0.0.0")
